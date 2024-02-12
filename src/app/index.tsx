737507f6b1220e71a0ad7439f4a7b443cf639613
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-02-12 22:47:20 +0000

    feat: header with cart and counter items, touchable button list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,16 @@
-import { Text, View } from "react-native";
-import tw from 'twrnc';
+import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
+import { View } from "react-native";
 
 export default function Home() {
   return (
 <View className="flex-1 pt-8">
-  <Text className="text-white text-2xl">
-    hello react native !
-  </Text>
+<Header title="Faça seu pedido" cartQuantityItems={3}/>
+  <View className="flex-row gap-4">
+    <CategoryButton title="Lanche do Dia" />
+    <CategoryButton title="Lanche do Dia" isSelected/>
+    <CategoryButton title="Lanche do Dia"/>
+  </View>
 </View>
 
   );
