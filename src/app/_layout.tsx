8ba870a319ae1d6fa4e0b3f9fbc356b97bd58c43
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-02-13 07:02:41 +0000

    (components)feat: add cart, product pages and link to whatsapp

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,31 +1,29 @@
-import { SafeAreaView} from "react-native"
 import { Slot } from "expo-router";
+import { SafeAreaView } from "react-native";
 import {
+  useFonts,
   Inter_400Regular,
   Inter_500Medium,
   Inter_600SemiBold,
   Inter_700Bold,
-} from "@expo-google-fonts/inter"
-import { useFonts } from "expo-font";
-import { Loading } from "@/components/loading"
+} from '@expo-google-fonts/inter'
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
- const [fontsLoaded] = useFonts(
-    {
-      Inter_400Regular,
-      Inter_500Medium,
-      Inter_600SemiBold,
-      Inter_700Bold,
-    }
-  )
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold,
+    Inter_700Bold,
+  })
 
-  if(!fontsLoaded){
-    return <Loading /> 
-  } 
+  if (!fontsLoaded) {
+    return <Loading />
+  }
 
   return (
-  <SafeAreaView className="flex-1 bg-slate-900">
-    <Slot />
-  </SafeAreaView>
-    )
+    <SafeAreaView className="flex-1 bg-slate-900">
+      <Slot />
+    </SafeAreaView>
+  )
 }
--- src/components/category-button.tsx
+++ src/components/category-button.tsx
@@ -10,7 +10,6 @@ export function CategoryButton({ title, isSelected, ...rest }: CategoryProps) {
   return (
     <Pressable
       className={clsx('bg-slate-800 px-4 justify-center rounded-md h-10', isSelected && "border-2 border-lime-300")}
-      // a propriedade rest agrega todas os atributos para a mudança de estados no react (não utilzar é pouco trivial)
       {...rest}
     >
       <Text className="text-slate-100 font-subtitle text-sm">{title}</Text>
