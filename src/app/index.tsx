abbe09cfe246ed5c355914b70636d1c60027d31a
Gabriel <132632103+bielaugustos@users.noreply.github.com>
2024-02-13 00:50:16 +0000

    (component)feat: add useState for  categoryButton component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
+import { CATEGORIES } from "@/app/utils/data/products"
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory : string) {
+    setCategory(selectedCategory)
+  }
+
   return (
 <View className="flex-1 pt-8">
 <Header title="Faça seu pedido" cartQuantityItems={3}/>
-  <View className="flex-row gap-4">
-    <CategoryButton title="Lanche do Dia" />
-    <CategoryButton title="Lanche do Dia" isSelected/>
-    <CategoryButton title="Lanche do Dia"/>
-  </View>
+    <FlatList 
+      data={CATEGORIES}
+      keyExtractor={(item) => item}
+      renderItem={({ item }) => (
+        <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)}/>
+        )}
+        horizontal
+        className="max-h-10 mt-5" 
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{gap: 12, paddingHorizontal: 20 }}
+      />
 </View>
 
   );
